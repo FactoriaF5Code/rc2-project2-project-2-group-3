08d5fec0c1129ce40f1a92de704f74d0e2aa395c
Johnny <jonathanbc2012@gmail.com>
2023-11-30 21:02:59 +0100

    menu update

---
+++ menu/mostrarP.js
@@ -0,0 +1,16 @@
+function mostrarParr() {
+    let parrOculto = document.querySelector('#parrOculto');
+    parrOculto.style.display = 'block';
+  }
+  
+
+  function mostrarParr2() {
+    let parrOculto = document.querySelector('#parrOculto2');
+    parrOculto.style.display = 'block';
+  }
+  
+  function mostrarParr3() {
+    let parrOculto = document.querySelector('#parrOculto3');
+    parrOculto.style.display = 'block';
+  }
+  
